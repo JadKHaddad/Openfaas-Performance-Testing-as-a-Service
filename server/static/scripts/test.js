78828dfdc3da6ed8bc64fb46b87dc864da221a20
jadkhaddad <jadkhaddad@gmail.com>
2021-09-23 23:24:46 +0200

    test.js/test.html fix

--- server/static/scripts/test.js
+++ server/static/scripts/test.js
@@ -1,23 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
     const id = document.getElementById('test-id').innerText;
-    // fetch('/test-info/'+id, { method: 'POST'}).then(res => res.json()).then(res => {
-    fetch(FUNCTIONCALL, {method:'POST', body: JSON.stringify({command: 8, id: id})}).then(res => res.json()).then(res => {
-        if (res.success){
+    function callBack(res) {
+        if (res.success) {
             const data = res.data;
             var users = null;
             var spawn_rate = null;
             var host = null;
-            var time =  null;
+            var time = null;
             const info = JSON.parse(data.info);
-            if (info != null){
+            if (info != null) {
                 users = info.users;
                 spawn_rate = info.spawn_rate;
                 host = info.host;
                 date = info.date;
                 time = info.time;
             }
-            const test = CreateTest(data.id, users, spawn_rate, host,time, data.status, data.code, data.data, data.valid, data.started_at);
+            const test = CreateTest(data.id, users, spawn_rate, host, time, data.status, data.code, data.data, data.valid, data.started_at);
             document.getElementById('content').appendChild(test);
         }
-    }).catch();
+    }
+    if (DIRECT) {
+        fetch(FUNCTIONCALL, { method: 'POST', body: JSON.stringify({ command: 8, id: id }) }).then(res => res.json()).then(res => {
+            callBack(res);
+        }).catch();
+    } else {
+        fetch('/test-info/' + id, { method: 'POST' }).then(res => res.json()).then(res => {
+            callBack(res);
+        }).catch();
+    }
 });
