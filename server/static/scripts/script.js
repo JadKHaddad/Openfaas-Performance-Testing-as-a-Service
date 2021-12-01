3224d2ce0b0951775632389fe4d11e7135836427
jadkhaddad <jadkhaddad@gmail.com>
2021-12-01 19:56:07 +0100

    host http validation with javascript

--- server/static/scripts/script.js
+++ server/static/scripts/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const users = $('#users-input').val();
         const spawnRate = $('#spawn-rate-input').val();
         const workers = $('#workers-input').val();
-        const host = $('#host-input').val();
+        const host = "http://" + $('#host-input').val().replace("http://", "").replace("https://", "");
         const time = $('#time-input').val();
         // handle false inputs
         if (users === '') {
