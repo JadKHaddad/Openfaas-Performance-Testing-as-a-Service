73f3f8cca34713700ee567e2171e715b0c7d67f3
jadkhaddad <jadkhaddad@gmail.com>
2021-12-04 13:57:29 +0100

    tester function added

--- script_deploy_faasd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#deploy function
-sudo cat /var/lib/faasd/secrets/basic-auth-password | faas-cli login -s
-cd /etc/Openfaas-Performance-Testing-as-a-Service/
-sudo faas-cli up -f ptas.yml
