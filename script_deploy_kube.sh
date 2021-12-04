c5dedec59712fde920ddcdf8b48044731f6eb5fa
jadkhaddad <jadkhaddad@gmail.com>
2021-12-04 13:45:27 +0100

    updates

--- script_deploy_kube.sh
+++ script_deploy_kube.sh
@@ -2,15 +2,12 @@
 
 while true
 do
+    #check if openfaas is ready
     if [ $(systemctl is-active port-forward.service) = "active" ]; then
-    touch /home/ubuntu/port_forward_running.txt
-#deploy function
-
+    #deploy function
     (sudo kubectl get secret -n openfaas basic-auth -o jsonpath="{.data.basic-auth-password}" | base64 --decode; echo) | sudo faas-cli login -s
     cd /etc/Openfaas-Performance-Testing-as-a-Service/
     sudo faas-cli up -f ptas.yml
-
-    touch /home/ubuntu/deployed.txt
     break
     fi
     sleep 2
--- script_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-touch /home/ubuntu/test.txt
-while true
-do
-    now=$(date +"%T")
-    printf "$now"| tee -a /home/ubuntu/test.txt
-    sleep 2
-done
