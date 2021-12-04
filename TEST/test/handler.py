93c10a71fee3dc61d08b92fe1fc8df9684a84c7a
jadkhaddad <jadkhaddad@gmail.com>
2021-12-04 13:50:56 +0100

    assets

--- TEST/test/handler.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-def handle(req):
-	p = subprocess.Popen(req,shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-	out, err = p.communicate()
-	return "out: " + str(out) + " | err: " + str(err)
