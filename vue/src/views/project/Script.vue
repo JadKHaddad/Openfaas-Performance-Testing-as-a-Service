5fe73c31a6cc4dae4b15113474a2946d00e38429
jadkhaddad <jadkhaddad@gmail.com>
2021-12-17 15:17:36 +0100

    dark theme

--- vue/src/views/project/Script.vue
+++ vue/src/views/project/Script.vue
@@ -46,12 +46,7 @@
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="ModalLabel">Start</h5>
-            <button
-              type="button"
-              class="btn-close"
-              data-mdb-dismiss="modal"
-              aria-label="Close"
-            ></button>
+            
           </div>
           <div class="modal-body">
             <form>
